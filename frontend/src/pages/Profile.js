2d22dfd706b36f3017aa19e92a90c27d358332f1
Adittya Mittal <adittya.mittal12@gmail.com>
2024-04-23 21:10:54 -0400

    basic profile page

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -7,62 +7,55 @@ import Navbar from "../components/Utilities/Navbar";
 import LargeHeading from "../components/Texts/LargeHeading";
 
 function Profile() {
-    // HOOKS
-    const dispatch = useDispatch();
+  // HOOKS
+  const dispatch = useDispatch();
 
-    // Get user acess and refresh token
-    const { accessToken, profileData } = useSelector(
-        (state) => state.UserReducer
-    );
+  // Get user acess and refresh token
+  const { accessToken, profileData } = useSelector(
+    (state) => state.UserReducer
+  );
 
-    // function to fetch profile data
-    const fetchProfileData = async () => {
-        // invoke a api call to get profile data
-        await authQueries.getProfile(accessToken, profileData, dispatch);
-    };
+  // function to fetch profile data
+  const fetchProfileData = async () => {
+    // invoke a api call to get profile data
+    await authQueries.getProfile(accessToken, profileData, dispatch);
+  };
 
-    // Get user information on load
-    useEffect(() => {
-        fetchProfileData();
-    }, []);
+  // Get user information on load
+  useEffect(() => {
+    fetchProfileData();
+  }, []);
 
-    // Check if profileData is null
-    if (!profileData) {
-      return <LargeHeading>Loading...</LargeHeading>;  // Or any other fallback UI
-    }
-    return (
-        <div className="h-[100vh] bg-black">
-            <ShowcaseContainer>
-                {/* Navbar */}
-                <Navbar />
+  // Check if profileData is null
+  if (!profileData) {
+    return <LargeHeading>Loading...</LargeHeading>;  // Or any other fallback UI
+  }
+  return (
+    <div className="min-h-screen bg-black">
+      <ShowcaseContainer>
+        {/* Navbar */}
+        <Navbar />
 
-                {/* Main */}
-                <div className="flex flex-row space-x-3">
-                    <div className="flex flex-1">
-                      {/* Profile Image */}
-                      <div>
-                      </div>
+        {/* Main */}
+        <div className="flex flex-col items-center justify-center h-full">
+          <div className="max-w-4xl mx-auto p-6 bg-black rounded-lg border border-gray-700">
+            <h3 className="text-2xl font-semibold mb-4 dark:text-white">User Details:</h3>
 
-                      {/* User Information */}
-                      <div className="ml-5">
-                        <LargeHeading>User Details:</LargeHeading>
-                        <div class="grid grid-cols-4 gap-4">
-                          <div>
-                            <p class="text-4xl text-gray-900 dark:text-white">First Name: {profileData.first_name}</p>
-                          </div>
-                          
-                          <div>09</div>
-                      </div>
-                      </div>
-
-
-                    </div>
-
-                    <div></div>
-                </div>
-            </ShowcaseContainer>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+              <div>
+                <p className="text-lg md:text-xl font-medium dark:text-white">First Name: <span className="font-normal dark:text-white">{profileData.first_name}</span></p>
+                <p className="text-lg md:text-xl font-medium mt-2 dark:text-white">Phone: <span className="font-normal dark:text-white">{profileData.phone}</span></p>
+              </div>
+              <div>
+                <p className="text-lg md:text-xl font-medium dark:text-white">Last Name: <span className="font-normal dark:text-white">{profileData.last_name}</span></p>
+                <p className="text-lg md:text-xl font-medium mt-2 dark:text-white">Email: <span className="font-normal dark:text-white">{profileData.email}</span></p>
+              </div>
+            </div>
+          </div>
         </div>
-    );
+      </ShowcaseContainer>
+    </div>
+  );
 }
 
 export default Profile;
