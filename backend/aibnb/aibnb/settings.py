0d228affe3e4a200b70ae2694f51621c68b5510e
Adittya Mittal <75125936+AM-git-hub@users.noreply.github.com>
2024-04-30 19:55:29 -0400

    Update settings.py

--- backend/aibnb/aibnb/settings.py
+++ backend/aibnb/aibnb/settings.py
@@ -80,7 +80,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'aibnb.wsgi.application'
+WSGI_APPLICATION = 'aibnb.wsgi.app'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
